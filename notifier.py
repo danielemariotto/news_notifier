37075463c3bbc795775b17645ed9277a6aae08fa
dan <daniele.mariotto@gmail.com>
2024-02-21 14:23:17 +0100

    send notification

---
+++ notifier.py
@@ -0,0 +1,9 @@
+from plyer import notification
+
+def send_notification(title, message):
+    notification.notify(
+        app_name='News Notifier',
+        title=title,
+        message=message,
+        timeout=10  # Display duration in seconds
+    )
